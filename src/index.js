c591b1a4d2e2cf773fa242a17b03a69858565026
Emil Mork <emil.a.mork@gmail.com>
2015-12-10 20:34:20 +0100

    Rename base function to Andor

--- src/index.js
+++ src/index.js
@@ -29,35 +29,34 @@ function or(a, ...rest) {
 
 function not(a) { return !a; }
 
-var Vercon = function Vercon(data) {
+var Andor = function(data) {
   d = data;
   this.ands = [];
   this.ors = [];
   return this;
 };
 
-Vercon.prototype.is = function (input, ...rest) {
+Andor.prototype.is = function (input, ...rest) {
   return this.and(input, ...rest);
 };
-Vercon.prototype.has = function(input, ...rest) {
+Andor.prototype.has = function(input, ...rest) {
   return this.and(input, ...rest);
 };
-Vercon.prototype.and = function(input, ...rest) {
+Andor.prototype.and = function(input, ...rest) {
   this.ands.push(and(input, ...rest));
   return this;
 };
-Vercon.prototype.or = function(input, ...rest) {
+Andor.prototype.or = function(input, ...rest) {
   this.ors.push(or(input, ...rest));
   return this;
 };
-Vercon.prototype.isTrue = function () {
+Andor.prototype.isTrue = function () {
   if (this.ors.length > 0) {
     return or(and(...this.ands), ...this.ors);
   }
 
   return and(...this.ands);
 };
-
 
 module.exports.and = and;
 module.exports.is = and;
@@ -66,5 +65,5 @@ module.exports.or = or;
 module.exports.not = not;
 
 module.exports.verifyThat = function(o) {
-  return new Vercon(o);
+  return new Andor(o);
 };
